5f0606a1abaafb6d505b8817c922a510f40c9f12
Max <114691428+maxmazal@users.noreply.github.com>
2025-04-04 14:14:54 -0400

    annotated BSTNode.cs

--- Assets/Scripts/BSTNode.cs
+++ Assets/Scripts/BSTNode.cs
@@ -1,5 +1,8 @@
 public class BSTNode
 {
+    //variables held for the node prefab
+    //value = value of the node (number)
+    //bstnode left and right; helps node keep track of what node is left and right of itself
     public int Value;
     public BSTNode Left;
     public BSTNode Right;
