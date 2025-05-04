ef081c3dc9c47d6bfde52789958b09197b2fbb32
MapleDrizzle <siromero@udel.edu>
2025-05-04 13:11:04 -0400

    Added simple submit button with popup messages

--- Assets/Scripts/BSTManager.cs
+++ Assets/Scripts/BSTManager.cs
@@ -1,27 +1,31 @@
 using UnityEngine;
+using UnityEngine.UI; //SR
 using TMPro;
 using System.Collections.Generic;
 
 public class BSTManager : MonoBehaviour
 {
     public static BSTManager instance;
-
+    // CREATION OF WIN STATE STARTS HERE V V
+    public enum GamePhase { Deletion, Insertion, Submission} // SR
+    public GamePhase currentPhase = GamePhase.Deletion; // SR
+    
     [SerializeField] public GameObject nodePrefab;
     [SerializeField] public Transform treeContainer;
     [SerializeField] private TMP_InputField inputField;
     [SerializeField] private TMP_Text scoreText;
-
     [SerializeField] private GameObject winScreen;
 
+    [SerializeField] private List<int> initialValues; // Added this field to the inspector
+    [SerializeField] private Button submitButton; // SR
 
     public Transform nodeParent;
     public BSTNode root;
     public Dictionary<int, GameObject> nodeObjects = new();
 
-    
-
     private int mistakeCount = 0;
     private int maxMistakes = 100;
+    private int nodesLeft = 2; //SR
 
     private void Awake()
     {
@@ -33,18 +37,27 @@ public class BSTManager : MonoBehaviour
 
     private void Start()
     {
-        Insert(10);
-        Insert(5);
-        Insert(15);
+        // Insert values from the initialValues list
+        foreach (var value in initialValues)
+        {
+            Insert(value);
+        }
+
         UpdateTree();
         UpdateLives();
     }
 
     public void InsertFromUI()
     {
+        if (currentPhase != GamePhase.Insertion) { // Make sure all invasive nodes gone
+            Debug.Log("You must delete all invasive nodes first."); // SR
+            return; // SR
+        }
         if (int.TryParse(inputField.text, out int value))
         {
             Insert(value);
+            currentPhase = GamePhase.Submission; //SR Changes GamePhase to know be submission
+            submitButton.interactable = true;
             inputField.text = "";
         }
         else
@@ -91,25 +104,30 @@ public class BSTManager : MonoBehaviour
         return node;
     }
 
-    // ✅ FIXED VERSION: no manual positions, reuses logic tree and updates all visuals after
-public GameObject InsertAt(BSTNodeBehavior parentNode, int value, bool isLeft)
-{
-    GameObject newNodeObj = Instantiate(nodePrefab, treeContainer);
-    var newBehavior = newNodeObj.GetComponent<BSTNodeBehavior>();
-    newBehavior.SetValue(value);
-    newBehavior.SetInvasive(Random.value < 0.4f);
+    public GameObject InsertAt(BSTNodeBehavior parentNode, int value, bool isLeft)
+    {
+        if (currentPhase != GamePhase.Insertion) // SR
+        {
+            Debug.Log("You must remove invasive nodes before inserting."); // SR
+            return null;
+        }
+        GameObject newNodeObj = Instantiate(nodePrefab, treeContainer);
+        var newBehavior = newNodeObj.GetComponent<BSTNodeBehavior>();
+        newBehavior.SetValue(value);
+        newBehavior.SetInvasive(false);
 
-    nodeObjects[value] = newNodeObj;
+        nodeObjects[value] = newNodeObj;
 
-    Vector3 offset = new Vector3(isLeft ? -1.5f : 1.5f, -1.5f, 0f);
-    newNodeObj.transform.position = parentNode.transform.position + offset;
+        Vector3 offset = new Vector3(isLeft ? -1.5f : 1.5f, -1.5f, 0f);
+        newNodeObj.transform.position = parentNode.transform.position + offset;
 
-    parentNode.ConnectChild(newBehavior, isLeft);
-    return newNodeObj;
-}
-
-
-
+        parentNode.ConnectChild(newBehavior, isLeft);
+        nodesLeft = nodesLeft - 1; //SR
+        if (nodesLeft == 0) { // SR
+            currentPhase = GamePhase.Submission; // Makes so now you can submit SR
+        }
+        return newNodeObj;
+    }
 
     public void UpdateTree()
     {
@@ -175,12 +193,27 @@ public GameObject InsertAt(BSTNodeBehavior parentNode, int value, bool isLeft)
             scoreText.text = "Game Over!";
         }
     }
+    // Helper Function for AttemptDeleteNode (checks if all invasive nodes are gone)
+    private bool AnyInvasiveNodesRemaining() // SR
+    {
+        foreach (var obj in nodeObjects.Values) { // SR
+            var behavior = obj.GetComponent<BSTNodeBehavior>(); // SR
+            if (behavior != null && behavior.isInvasive) //SR
+                return true; //SR
+        }
+        return false; //SR
+    }
 
     public void AttemptDeleteNode(BSTNodeBehavior nodeBehavior)
     {
+        if (currentPhase != GamePhase.Deletion) //SR
+        {
+            MessagePopup.instance.ShowMessage("Finish inserting!"); //SR
+            return;
+        }
         if (nodeBehavior.isInvasive)
         {
-            Debug.Log("Invasive node deleted!");
+            MessagePopup.instance.ShowMessage("Invasive node deleted!"); // ALL DEBUG MESSAGES NOW POPUP
 
             foreach (var obj in nodeObjects.Values)
             {
@@ -204,15 +237,20 @@ public GameObject InsertAt(BSTNodeBehavior parentNode, int value, bool isLeft)
             {
                 SFXScript.instance.PlayInsertSound();
             }
+            if(!AnyInvasiveNodesRemaining()) //SR
+            { 
+                currentPhase = GamePhase.Insertion; //SR
+                MessagePopup.instance.ShowMessage("All invasive nodes removed! Now insert healthy nodes."); //SR
+            }
         }
         else
         {
-            Debug.Log("Healthy node deleted. Mistake made.");
+            MessagePopup.instance.ShowMessage("Healthy node deleted. You lost a life!");
             mistakeCount++;
             UpdateLives();
             if (mistakeCount >= maxMistakes)
             {
-                Debug.Log("Game Over.");
+                MessagePopup.instance.ShowMessage("Game Over.");
             }
         }
     }
@@ -250,40 +288,58 @@ public GameObject InsertAt(BSTNodeBehavior parentNode, int value, bool isLeft)
         return node;
     }
 
-public void OnSubmitTree()
-{
-    if (IsValidBST(root, int.MinValue, int.MaxValue))
+    public void OnSubmitTree()
     {
-        Debug.Log("Tree is valid!");
-        if (winScreen != null) winScreen.SetActive(true);
+        Debug.Log("Submit button clicked!");
+        if (currentPhase != GamePhase.Submission) //SR
+        {
+            MessagePopup.instance.ShowMessage("Finish inserting before submitting."); //SR
+            return;
+        }
+        if (IsValidBST(root, int.MinValue, int.MaxValue))
+        {
+            MessagePopup.instance.ShowMessage("Tree is valid! You saved the tree :)");
+            if (winScreen != null) winScreen.SetActive(true);
+        }
+        else
+        {
+            MessagePopup.instance.ShowMessage("Invalid BST. Resetting tree.");
+            ResetTree();
+        }
     }
-    else
+
+    private bool IsValidBST(BSTNode node, int min, int max)
     {
-        Debug.Log("Invalid BST. Resetting tree.");
-        ResetTree();
+        if (node == null) return true;
+        if (node.Value <= min || node.Value >= max) return false;
+
+        return IsValidBST(node.Left, min, node.Value) &&
+               IsValidBST(node.Right, node.Value, max);
     }
-}
 
-
-private bool IsValidBST(BSTNode node, int min, int max)
-{
-    if (node == null) return true;
-    if (node.Value <= min || node.Value >= max) return false;
-
-    return IsValidBST(node.Left, min, node.Value) &&
-           IsValidBST(node.Right, node.Value, max);
-}
-
-public void ResetTree()
-{
-    foreach (var obj in nodeObjects.Values)
+    public void ResetTree()
     {
-        Destroy(obj);
-    }
-    nodeObjects.Clear();
-    root = null;
-    mistakeCount++;
-    UpdateLives();
-}
+        submitButton.interactable = false;
+        foreach (var obj in nodeObjects.Values)
+        {
+            Destroy(obj);
+        }
+        nodeObjects.Clear();
+        root = null;
+        mistakeCount++;
+        UpdateLives();
 
+        if (mistakeCount < maxMistakes) //SR
+        {
+            currentPhase = GamePhase.Deletion; //SR
+            foreach (var value in initialValues) //SR
+                Insert(value);
+
+            UpdateTree();
+        }
+        else
+        {
+            MessagePopup.instance.ShowMessage("Game Over. No more lives.");
+        }
+    }
 }
